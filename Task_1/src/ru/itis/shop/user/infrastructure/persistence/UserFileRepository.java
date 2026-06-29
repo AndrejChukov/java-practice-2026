17e89dd0492b0a88993230ed6e69608f6fe68e98
Andrej <andrej_chuchkalov@mail.ru>
2026-06-29 23:42:59 +0300

    first commit

---
+++ Task_1/src/ru/itis/shop/user/infrastructure/persistence/UserFileRepository.java
@@ -0,0 +1,48 @@
+package ru.itis.shop.user.infrastructure.persistence;
+
+import ru.itis.shop.user.domain.User;
+import ru.itis.shop.user.repository.UserRepository;
+
+import java.io.*;
+import java.util.Arrays;
+import java.util.UUID;
+
+public class UserFileRepository implements UserRepository {
+
+    private final String fileName;
+
+    public UserFileRepository(String fileName) {
+        this.fileName = "Task_1/" + fileName;
+    }
+
+    @Override
+    public void save(User user) {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(fileName, true))) {
+            String id = UUID.randomUUID().toString();
+            user.setId(id);
+            writer.write(user.getId() + "|" +
+                    user.getEmail() + "|" +
+                    user.getPassword() + "|" +
+                    user.getProfileDescription());
+            writer.newLine();
+        } catch (IOException e) {
+            throw new IllegalStateException(e);
+        }
+    }
+
+    @Override
+    public User findById(String id) {
+        try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                String[] data = line.trim().split("\\|");
+                if (data[0].equals(id)) {
+                    return new User(data[0], data[1], data[2], data[3]);
+                }
+            }
+        } catch(IOException e) {
+            throw new IllegalStateException(e);
+        }
+        return null;
+    }
+}
